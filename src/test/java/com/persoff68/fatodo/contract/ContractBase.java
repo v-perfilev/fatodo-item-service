7c05e2a11887c2c2ddec8a8a50c0f86d90487554
Vladimir Perfilev <persoff68@gmail.com>
2020-04-17 19:59:56 +0200

    TDM-28 add contracts

--- src/test/java/com/persoff68/fatodo/contract/ContractBase.java
+++ src/test/java/com/persoff68/fatodo/contract/ContractBase.java
@@ -1,11 +1,22 @@
 package com.persoff68.fatodo.contract;
 
+import com.persoff68.fatodo.FactoryUtils;
+import com.persoff68.fatodo.client.GroupServiceClient;
+import com.persoff68.fatodo.model.Item;
+import com.persoff68.fatodo.model.constant.ItemStatus;
+import com.persoff68.fatodo.repository.ItemRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMessageVerifier
@@ -13,10 +24,24 @@ public abstract class ContractBase {
 
     @Autowired
     WebApplicationContext context;
+    @Autowired
+    ItemRepository itemRepository;
+
+    @MockBean
+    GroupServiceClient groupServiceClient;
 
     @BeforeEach
     public void setup() {
         RestAssuredMockMvc.webAppContextSetup(context);
+        itemRepository.deleteAll();
+        Item item = FactoryUtils.createItem("1", "test_group_id", ItemStatus.ACTIVE);
+        item.setId("test_id_1");
+        itemRepository.save(item);
+
+        when(groupServiceClient.getAllGroupIdsForUser()).thenReturn(List.of("test_group_id"));
+        when(groupServiceClient.canRead(any())).thenReturn(true);
+        when(groupServiceClient.canEdit(any())).thenReturn(true);
+        when(groupServiceClient.canAdmin(any())).thenReturn(true);
     }
 
 }
---
+++ src/test/resources/contracts/itemresource/get.groovy
@@ -0,0 +1,34 @@
+package contracts.itemresource
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    name 'get item by Id'
+    description 'should return status 200 and ItemDTO'
+    request {
+        method GET()
+        url($(
+                consumer(regex('\\/api\\/items\\/[\\w-]+')),
+                producer("/api/items/test_id_1")
+        ))
+        headers {
+            header 'Authorization': $(
+                    consumer(containing("Bearer")),
+                    producer("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzIiwidXNlcm5hbWUiOiJ0ZXN0X3VzZXIiLCJhdXRob3JpdGllcyI6IlJPTEVfVVNFUiIsImlhdCI6MCwiZXhwIjozMjUwMzY3NjQwMH0.ggV38p_Fnqo2OZNtwR3NWKZhMXPd-vf4PrRxN0NmTWsHPrKwWZJSGO2dJBBPWXWs4OI6tjsNV2TM3Kf6NK92hw")
+            )
+        }
+    }
+    response {
+        status 200
+        headers {
+            contentType applicationJson()
+        }
+        body(
+                "id": "test_id_1",
+                "title": "test_title_1",
+                "body": "test_body_1",
+                "status": "ACTIVE",
+                "groupId": "test_group_id"
+        )
+    }
+}
