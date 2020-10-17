1dbaf4466288be883341cb4b68ecd440e5d33164
Vladimir Perfilev <persoff68@gmail.com>
2020-10-17 17:19:27 +0200

    TDM-70 edit repos

--- src/main/java/com/persoff68/fatodo/repository/ItemRepository.java
+++ src/main/java/com/persoff68/fatodo/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package com.persoff68.fatodo.repository;
 import com.mongodb.lang.NonNull;
 import com.persoff68.fatodo.config.aop.cache.annotation.CacheEvictMethod;
 import com.persoff68.fatodo.config.aop.cache.annotation.CacheableMethod;
-import com.persoff68.fatodo.config.aop.cache.annotation.MultiCacheEvictMethod;
 import com.persoff68.fatodo.model.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -23,24 +22,18 @@ public interface ItemRepository extends MongoRepository<Item, String> {
     Optional<Item> findById(@NonNull String id);
 
     @Override
-    @MultiCacheEvictMethod({
-            @CacheEvictMethod(cacheName = "items-by-id", key = "#item.id"),
-            @CacheEvictMethod(cacheName = "items-by-group-id", key = "#item.groupId")
-    })
+    @CacheEvictMethod(cacheName = "items-by-id", key = "#item.id")
+    @CacheEvictMethod(cacheName = "items-by-group-id", key = "#item.groupId")
     @NonNull
     <S extends Item> S save(@NonNull S item);
 
 
     @Override
-    @MultiCacheEvictMethod({
-            @CacheEvictMethod(cacheName = "items-by-id", key = "#item.id"),
-            @CacheEvictMethod(cacheName = "items-by-group-id", key = "#item.groupId")
-    })
+    @CacheEvictMethod(cacheName = "items-by-id", key = "#item.id")
+    @CacheEvictMethod(cacheName = "items-by-group-id", key = "#item.groupId")
     void delete(@NonNull Item item);
 
-    @MultiCacheEvictMethod({
-            @CacheEvictMethod(cacheName = "items-by-id", key = "#id"),
-            @CacheEvictMethod(cacheName = "items-by-group-id", key = "#groupId")
-    })
+    @CacheEvictMethod(cacheName = "items-by-id", key = "#id")
+    @CacheEvictMethod(cacheName = "items-by-group-id", key = "#groupId")
     void deleteAllByIdInAndGroupId(List<String> id, String groupId);
 }
