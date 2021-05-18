b1603999caf4d23ca54fcc9dbfe52ffc4aa1f62a
Vladimir Perfilev <persoff68@gmail.com>
2021-05-18 12:53:12 +0200

    TDM-88 fix item service client

--- src/test/resources/contracts/checkresource/isGroup.groovy
+++ src/test/resources/contracts/checkresource/isGroup.groovy
@@ -12,7 +12,6 @@ Contract.make {
                 producer("/api/check/is-group/12886ad8-f1a2-487c-a5f1-ff71d63a3b52")
         ))
         headers {
-            contentType applicationJson()
             header 'Authorization': $(
                     consumer(containing("Bearer")),
                     producer("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI4ZjlhN2NhZS03M2M4LTRhZDYtYjEzNS01YmQxMDliNTFkMmUiLCJ1c2VybmFtZSI6InRlc3RfdXNlciIsImF1dGhvcml0aWVzIjoiUk9MRV9VU0VSIiwiaWF0IjowLCJleHAiOjMyNTAzNjc2NDAwfQ.Go0MIqfjREMHOLeqoX2Ej3DbeSG7ZxlL4UAvcxqNeO-RgrKUCrgEu77Ty1vgR_upxVGDAWZS-JfuSYPHSRtv-w")
