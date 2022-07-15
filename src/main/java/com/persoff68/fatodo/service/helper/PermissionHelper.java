a19d6cfc787b33c864647b1f555a5f1655a50a35
Vladimir Perfilev <persoff68@gmail.com>
2022-07-15 15:22:27 +0200

    TDM-149 fix code smells

--- src/main/java/com/persoff68/fatodo/service/helper/PermissionHelper.java
+++ src/main/java/com/persoff68/fatodo/service/helper/PermissionHelper.java
@@ -1,14 +1,12 @@
 package com.persoff68.fatodo.service.helper;
 
 import com.persoff68.fatodo.model.Group;
-import com.persoff68.fatodo.model.Member;
 import com.persoff68.fatodo.model.constant.Permission;
 import com.persoff68.fatodo.security.exception.UnauthorizedException;
 import com.persoff68.fatodo.security.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.UUID;
 
 @Component
